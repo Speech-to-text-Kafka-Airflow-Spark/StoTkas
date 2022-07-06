539dcdaad2766d6afb3d5f2189bae145afe7a70a
Samrawit02 <hairaer02@gmail.com>
2022-07-06 22:36:34 +0300

    script for creating kafka topic

---
+++ scripts/create_topic.py
@@ -0,0 +1,8 @@
+from kafka import KafkaConsumer
+from kafka.admin import KafkaAdminClient, NewTopic
+admin_client = KafkaAdminClient(bootstrap_servers="b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092", client_id='g1')
+
+topic_list = []
+topic_list.append(NewTopic(name="Chang", num_partitions=1, replication_factor=2))
+topic_list.append(NewTopic(name="chang_audio", num_partitions=1, replication_factor=2))
+admin_client.create_topics(new_topics=topic_list, validate_only=False)
