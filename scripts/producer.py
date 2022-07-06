41d369d14f873457e425c3a7eed24e30b849d584
Samrawit02 <hairaer02@gmail.com>
2022-07-06 23:12:33 +0300

    adding the producer

--- scripts/producer.py
+++ scripts/producer.py
@@ -1,8 +1,32 @@
-from kafka import KafkaConsumer
-from kafka.admin import KafkaAdminClient, NewTopic
-admin_client = KafkaAdminClient(bootstrap_servers="b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092", client_id='chang')
+import json
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import pandas as pd
+from create_topic import Topics
+class Producer:
+    """ Create a producer and emit events from s3 bucket"""
 
-topic_list = []
-topic_list.append(NewTopic(name="chang", num_partitions=1, replication_factor=2))
-topic_list.append(NewTopic(name="chang_audio", num_partitions=1, replication_factor=2))
-admin_client.create_topics(new_topics=topic_list, validate_only=False)
+    def __init__(self, bootstrap='b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092', create_topic=True, topic='transc_data'):
+        self.bootstrap = bootstrap
+        self.topic = topic
+        self.create_topic = create_topic
+  
+
+    def start_publishing(self):
+        """ Start producer by initializing producer"""
+        self.get_data()
+        self.initialize()
+        if self.create_topic:
+            tp = Topics('Benkart', [self.topic])
+            tp.create_topics()
+            self.topic = tp.topics[0]
+        self.publish()
+
+    def initialize(self):
+        """ Initialize producer with bootstrap server """
+        self.producer = KafkaProducer(
+            bootstrap_servers=self.bootstrap,
+            api_version=(0, 10, 1),
+            value_serializer=lambda v: json.dumps(v).encode('UTF-8'),
+            key_serializer=lambda v: json.dumps(v).encode('UTF-8')
+        )
