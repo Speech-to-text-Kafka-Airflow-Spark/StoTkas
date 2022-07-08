5e8e21f9ddf408978fb3fbe09465884037ee1eb4
group one <ryfgmz@gmail.com>
2022-07-08 17:58:44 +0000

    change

--- scripts/create_topic.py
+++ scripts/create_topic.py
@@ -9,7 +9,7 @@ class Topics():
         admin_client = KafkaAdminClient(
         bootstrap_servers="localhost:9092", 
         client_id='group1'
-)
+    )
 
         topic_list = []
         topic_list.append(NewTopic(name="example_topic", num_partitions=1, replication_factor=1))
--- scripts/producer.py
+++ scripts/producer.py
@@ -4,3 +4,4 @@ from kafka.errors import KafkaError
 def createProducer():
     producer = KafkaProducer(bootstrap_servers=['b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092'])
     return producer
+createProducer()
