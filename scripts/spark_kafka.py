188f27bb0b0e78cde1fa4b43bda51213bf8a71a5
sam23121 <smlalene@gmail.com>
2022-07-09 20:47:03 +0300

    add script

--- scripts/spark_kafka.py
+++ scripts/spark_kafka.py
@@ -38,17 +38,3 @@ query = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
 
 
 query.awaitTermination()
-
-df.printSchema()
-
-# def upload_file(file_name: str, bucket: str, object_name=None):
-#     # If S3 object_name was not specified, use file_name
-#     if object_name is None:
-#         object_name = os.path.basename(file_name)
-
-#     # Upload the file
-#     s3_client = boto3.client('s3')
-    
-#     response = s3_client.upload_file(file_name, bucket, object_name)
-    
-#     return True
