7b4d4b703493e68d2002f44224faa4de3fb3030b
Yonas-T <yonaztad@gmail.com>
2022-07-09 08:38:39 +0000

    adjutment on bucket

--- scripts/bucket.py
+++ scripts/bucket.py
@@ -4,29 +4,20 @@ from botocore.exceptions import ClientError
 
 
 class Bucket:
-    def __init__():
-        pass
+    def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
 
-    def create_bucket(bucket_name, region=None):
-        """Create an S3 bucket in a specified region
-
-        If a region is not specified, the bucket is created in the S3 default
-        region (us-east-1).
-
-        :param bucket_name: Bucket to create
-        :param region: String region to create bucket in, e.g., 'us-west-2'
-        :return: True if bucket created, else False
-        """
-
+    def create_bucket(self, region=None):
+        
         # Create bucket
         try:
             if region is None:
                 s3_client = boto3.client('s3')
-                s3_client.create_bucket(Bucket=bucket_name)
+                s3_client.create_bucket(Bucket=self.bucket_name)
             else:
                 s3_client = boto3.client('s3', region_name=region)
                 location = {'LocationConstraint': region}
-                s3_client.create_bucket(Bucket=bucket_name,
+                s3_client.create_bucket(Bucket=self.bucket_name,
                                         CreateBucketConfiguration=location)
         except ClientError as e:
             logging.error(e)
